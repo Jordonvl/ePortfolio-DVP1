490550a3fc987c01e0157020f039692c1f8331de
jordon van leenen <jordo@jordons-MacBook-Pro.local>
2018-07-16 21:30:37 -0400

    First commit for challenge 1

---
+++ DVP1.CE1/DVP1.CE1/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Jordon van Leenen
+//1807
+//Project & Portfolio
+//In this coding challenge I am creating a welcoming menu where the user starts.
+namespace DVP1.CE1
+{
+    class Menu
+    {
+    }
+}
---
+++ DVP1.CE1/DVP1.CE1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Jordon van Leenen
+//1807
+//Project & Portfolio 1
+//This coding project is going to contain all my challenges from portfolio 1
+namespace DVP1.CE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
