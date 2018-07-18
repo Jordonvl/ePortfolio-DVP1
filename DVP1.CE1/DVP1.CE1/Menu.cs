41a39f82cbecf01f8cc3f204df4a1e2ca2074380
jordon van leenen <jordo@jordons-MacBook-Pro.local>
2018-07-18 00:27:55 -0400

    This commit I added the switch statement to be used as conditionals that go through all the options

--- DVP1.CE1/DVP1.CE1/Menu.cs
+++ DVP1.CE1/DVP1.CE1/Menu.cs
@@ -12,38 +12,75 @@ namespace DVP1.CE1
     class Menu
     {
         static int menuChoice = 0;
-    }
 
-    public static void GetMenu()
-    {
-        while (true)
+
+        public static void GetMenu()
         {
-            Console.Clear();
-            Console.WriteLine("Menu");
-            Console.WriteLine("Enter valid number choice for challenge you want to run");
-            Console.WriteLine("");
-            Console.WriteLine("[1] Swap Name");
-            Console.WriteLine("[2] Backwards");
-            Console.WriteLine("[3] Age Convert");
-            Console.WriteLine("[4] Temp Convert");
-            Console.WriteLine("[5] Big Blue Fish");
-            Console.WriteLine("");
-            Console.WriteLine("[0] Exit");
-            Console.WriteLine("");
-            Console.WriteLine("Enter your slection");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu");
+                Console.WriteLine("Enter valid number choice for challenge you want to run");
+                Console.WriteLine("");
+                Console.WriteLine("[1] Swap Name");
+                Console.WriteLine("[2] Backwards");
+                Console.WriteLine("[3] Age Convert");
+                Console.WriteLine("[4] Temp Convert");
+                Console.WriteLine("[5] Big Blue Fish");
+                Console.WriteLine("");
+                Console.WriteLine("[0] Exit");
+                Console.WriteLine("");
+                Console.WriteLine("Enter your slection");
+
+                switch (GetValidation(menuChoice))
+                {
+                    case 1:
+                        {
+
+
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Not ready yet");
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Not ready yet");
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Not ready yet");
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Not ready yet");
+                        }
+                        break;
+                    case 0:
+                        {
+                            Console.WriteLine();
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
         }
 
-        
-    }
-
-    public static int GetValidation(int menuChoiceInt)
-    {
-        string menuInput = Console.ReadLine();
-
-        while (!int.TryParse(menuInput, out menuChoiceInt) && menuChoiceInt < 0 && menuChoiceInt > 5)
+        public static int GetValidation(int menuChoiceInt)
         {
-            Console.WriteLine("Please enter one of the choice in the menu");
+            string menuInput = Console.ReadLine();
+
+            while (!int.TryParse(menuInput, out menuChoiceInt) && menuChoiceInt < 0 && menuChoiceInt > 5)
+            {
+                Console.WriteLine("Please enter one of the choice in the menu");
+            }
+            return menuChoiceInt;
         }
-        return menuChoiceInt;
     }
 }
