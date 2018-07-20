b883213432363c98ab0b3b7c3be465184d687e59
jordon van leenen <jordo@jordons-MBP.centurylink.net>
2018-07-20 12:05:22 -0400

    Added a Bakwards class and prompted the user for the 6 worded sentence and stored their response, as well as displaying their response in the console.

---
+++ DVP1.CE1/DVP1.CE1/Backwards.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Jordon van Leenen
+//1807
+//Project & Portfolio
+//In this coding challenge I am creating a class where I reverse the order of a sentence and reverse the words themselves.
+namespace DVP1.CE1
+{
+    class Backwards
+    {
+        public static void GetBackWards()
+        {
+            Console.WriteLine("Enter a sentence containing at least 6 words");
+            string sentenceString = Console.ReadLine();
+
+            Console.WriteLine($"Thank you, you entered the sentence \r\n {sentenceString}");
+        }
+    }
+}
